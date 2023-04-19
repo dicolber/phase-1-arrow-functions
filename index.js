a69f9488669033a0354db190009b07d3a5ab35b7
dicola berry <dicolber@amazon.com>
2023-04-18 20:15:40 -0400

    arrow functions

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+  return 2000/100
+}
+
+const square = (parameter1) => parameter1 * parameter1
+square(1)
+
+const add = (parameter2,parameter3) => parameter2 + parameter3
+add(2,3)
